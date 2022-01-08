d0099e30117e5b55a3dcface359a222c5bc015b1
rayenebech <rayenebech@gmail.com>
2022-01-08 22:56:16 +0300

    express added

--- server/index.js
+++ server/index.js
@@ -1,22 +1,42 @@
 const express = require('express');
-//const cors = require('cors');
-
 const app = express();
-
-//const index = require('./routes/index');
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
-//app.use(express.json({ type: 'application/vnd.api+json' }));
-//app.use(cors());
-//app.use(index);
+const path = require('path')
 
 
+ app.use(express.static('../client'))
+ 
+//Login page
 app.get('/', function(req,res){
-    res.send('Home Page');
+    res.sendFile(path.resolve('../client/login.html'))
+    //res.status(200).send('Login Form');
 })
-app.get('/about', function(req,res){
-    res.send('About page');
+/*****************  Saler Interfaces **************/
+// Products Management
+//Stock Management
+//Statistical Reports
+/**************************************************/
+
+
+
+/*****************  Viewer Interfaces **************/
+// Products Page
+app.get('/products', function(req,res){
+    res.status(200).send('products page');
 })
+//Market Page
+app.get('/market', function(req,res){
+    res.status(200).send('market page');
+})
+/*************************************************/
+
+
+/** Handle any other route**/
+
+app.all('*', (req,res) =>{
+    res.status(404).send(`<h1> 404 Error: Page not found <h1>`)
+})
+
+
 var server = app.listen(5001, function (){
     var host = server.address().address;
     var port = server.address().port;
