c2d5e4d23412a26ef786ad310f0f735f4e879d09
rayenebech <rayenebech@gmail.com>
2022-01-13 21:52:49 +0300

    routes and database

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,16 +7,17 @@ const bcrypt = require('bcryptjs')
 router.post('/', async (req,res) =>{
     try{
         const { username, password} = req.body;
-
-        /*Encrypt
-            const hashed_password = await bcrypt.hash(password, 10);
-
+        /*const pass1 = await bcrypt.hash("112233", 10)
+        const pass2 = await bcrypt.hash("112233", 10)
+        console.log(pass1)
+        console.log(pass2)
+        const hashed_password = await bcrypt.hash(password, 10);
         */
-
-        const user = await pool.query(
+       const user = await pool.query(
             "SELECT * FROM mylogin($1, $2)", 
             [username.toLowerCase(), password]
           );
+        console.log(user)
         const result = user.rows[0]['mylogin']
         if(result==-1){
             res.status(401).json("Login failed. Please check again your information")
--- server/routes/sellers.js
+++ server/routes/sellers.js
@@ -8,10 +8,11 @@ const router = express.Router();
 router.get('/:sellerId/stocks', async (req, res) => {
   try {
     const { sellerId } = req.params;
+    console.log(sellerId)
     const allStocks = await pool.query("SELECT * FROM list_stock ($1)",
     [sellerId]
     );
-    console.log(allStocks)
+    //console.log(allStocks)
     res.status(200).json(allStocks.rows);
   } catch (err) {
     console.error(err.message);
@@ -37,19 +38,21 @@ router.get('/:sellerId/productsNotInStock', async (req, res) => {
 
 
 // Add a new product to the stock
+/**********************************************Check it ************************************************** */
+/**Does not gþcew warning for negative senario */
 router.post('/:sellerId/addStock', async (req, res) => {
   const { sellerId } = req.params;
   const product_id = req.body.product_id;
   const quantity = req.body.quantity;
   const price = req.body.price;
+  console.log(req.body)
   try {
     const newStock = await pool.query(
-      `SELECT * from add_stock ($1, $2, $3, $4)`,
+      `SELECT * FROM add_stock ($1, $2, $3, $4)`,
       [sellerId, product_id, quantity, price ]);
-    console.log( [newStock.rows[0]])
-    res.status(200).json(`Product was successfully added with id: ${new_id}`);
-    /*
-    new_id = [newStock.rows[0]['productid']]
+    console.log( [newStock])
+    res.status(200).json(`Product was successfully added`)
+    /*new_id = [newStock.rows[0]['productid']]
     if( new_id == -1){
         res.status(409).json("Product already exists")
     }
@@ -64,21 +67,52 @@ router.post('/:sellerId/addStock', async (req, res) => {
 
 
 
-/*
-// Delete Product
-router.delete('/:id', async (req, res) => {
+
+// Delete a product from the stock
+router.delete('/:sellerId/deleteFromStock', async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleteProduct = await pool.query("DELETE FROM product WHERE id = $1", [
-      id
+    const { seller_id } = req.params;
+    const { product_id } = req.body;
+    const deleteFromStock = await pool.query("SELECT * FROM remove_stock ($1 , $2 ) ", [
+      seller_id, product_id
     ]);
-    res.json("Product was deleted!");
+    const deleted = deleteFromStock.rows[0]
+    console.log(deleteFromStock)
+    res.status(200).json(`Product was successfully deleted from stock`);
+    /*new_id = [deleteFromStock.rows[0]['productid']]
+    if( new_id == -1){
+        res.status(409).json("Product already exists")
+    }
+    else{
+      res.status(200).json(`Product was successfully added with id: ${new_id}`);
+    }*/
   } catch (err) {
     console.log(err.message);
   }
 });
 
 
-*/
 
+
+// Delete a product from the stock
+router.put('/:sellerId/updateStock', async (req, res) => {
+  try {
+    const { seller_id } = req.params;
+    const product_id = req.body.product_id;
+    const quantity = req.body.quantity;
+    const updateStock = await pool.query("SELECT * FROM update_quantity ($1 , $2, $3 ) ", [
+      seller_id, product_id, quantity
+    ]);
+    console.log(updateStock)
+    const result  = [updateStock.rows[0]['update_quantity']]
+    if( result == -1){
+        res.status(409).json("Product couldn't be updated ")
+    }
+    else{
+      res.status(200).json(`Product was successfully updated`);
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 module.exports = router;
